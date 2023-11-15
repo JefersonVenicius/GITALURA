da0fe9e4a9691f31579d08516b921796544f201e
Venicius Vieira <veniciusvieira90@gmail.com>
2023-11-15 19:39:48 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
 // mais um cosole log de teste
 console.log("modificação")
+console.log("Enviando para o GITHUB")
